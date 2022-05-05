c23f609fad561509105db75605a50b6a86a4765b
AntonLip <liplianin.anton@yandex.ru>
2022-05-05 21:47:34 +0300

    start

--- AgroBot.Models/Bot.cs
+++ AgroBot.Models/Bot.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AgroBot.Models.Commands;
+using AgroBot.Models.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,20 @@ using System.Threading.Tasks;
 
 namespace AgroBot.Models
 {
-    class Bot
+    public class Bot : IBot
     {
+        private List<ICommand> commandsList;
+        public Bot( )
+        {
+            commandsList = new List<ICommand>();
+            commandsList.Add(new StartCommand());
+        }
+
+        public List<ICommand> GetCommands()
+        {
+            if (commandsList is null)
+                throw new System.Exception();
+            return commandsList;
+        }
     }
 }
